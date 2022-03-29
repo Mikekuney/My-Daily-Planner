93f2e75669fed7879ff11eed61b68e28b4393918
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-03-28 23:08:07 -0400

    updated timeblock stored data and css class changing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-
+var currentHour = moment().format("HH")
+var workHour = moment().startOf('day').add(7, "hour")
+$("#currentDay").text(moment().format("dddd, MMMM, do YYYY"))
 
 // function with for loop to create timeblocks
 var dayHours = function() {
@@ -9,5 +11,28 @@ var dayHours = function() {
     // loop to create Timeblocks
     for(var i =8; i < 17; i++) {
         var workDay = workHour.add(1, "hour").format("h A");
+
+        // Update timeblock textarea 
+        var key = "hour" + i;
+        timeMap[key] = timeMap[key] || null;
+
+        var row = $("<div></div>").addClass("row time-block");
+        var hour = $("</div></div>").addClass("col-1 hour");
+        var textInputEl = $("<textarea></textarea>").addClass("col-10 textInputClass").val(timeMap[key]);
+        var saveBtn = $("<button></button>").addClass("col-1 fa fa-save saveBtn").attr("key", key);
+
+        $(".container").append(row);
+        $(row).append(hour.textInputEl,saveBnt);
+
+        $(hour).text(workDay);
+
+        // change css class styling when conditions are met
+        if(i < currentHour) {
+            $(textInputEl).attr("class", "col-10 row past")
+        } else if(i > currentHour) {
+            $(textInputEl).attr("class", "col-10 row future")
+        } else {
+            $(textInputEl).attr("class", "col-10 row present")
+        }
     }
 }
